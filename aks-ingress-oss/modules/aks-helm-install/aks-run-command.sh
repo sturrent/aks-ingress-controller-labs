8d349f0554f48f4997b22a2af0fbef2b17cc6917
sturrent <romeo.turrent@gmail.com>
2025-07-09 16:02:52 -0600

    Fix quoting issues in aks-run-command.sh for improved variable handling

--- aks-ingress-oss/modules/aks-helm-install/aks-run-command.sh
+++ aks-ingress-oss/modules/aks-helm-install/aks-run-command.sh
@@ -10,7 +10,7 @@ export LANG=C.UTF-8
 if [ "$initialDelay" != "0" ]
 then
     echo "Waiting on RBAC replication ($initialDelay)"
-    sleep $initialDelay
+    sleep "$initialDelay"
 
     #Force RBAC refresh
     az logout
@@ -21,12 +21,12 @@ fi
 helmValuesFileName="$(dirname "$0")/helmvalues.yaml"
 
 echo "Creating Helm values file..."
-echo -n $helmAppValues | base64 -d > $helmValuesFileName
+echo -n $helmAppValues | base64 -d > "$helmValuesFileName"
 echo "Helm values file created and stored at $helmValuesFileName"
 
 echo "Sending command to AKS Cluster $aksName in $RG"
-cmdOut=$(az aks command invoke -g $RG -n $aksName  --command "helm repo add ${helmRepo} ${helmRepoURL} && helm repo update && helm upgrade --install ${helmAppName} ${helmApp} ${helmAppParams} -f $helmValuesFileName" --file $helmValuesFileName -o json)
-echo $cmdOut
+cmdOut=$(az aks command invoke -g "$RG" -n "$aksName" --command "helm repo add ${helmRepo} ${helmRepoURL} && helm repo update && helm upgrade --install ${helmAppName} ${helmApp} ${helmAppParams} -f $helmValuesFileName" --file "$helmValuesFileName" -o json)
+echo "$cmdOut"
 
-jsonOutputString=$cmdOut
-echo $jsonOutputString > $AZ_SCRIPTS_OUTPUT_PATH
+jsonOutputString=$(jq -n --arg output "$cmdOut" '{output: $output}')
+echo "$jsonOutputString" > "$AZ_SCRIPTS_OUTPUT_PATH"
